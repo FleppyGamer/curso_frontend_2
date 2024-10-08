5f9fd167f09b73ac1284db28e68d844d3f96ffbe
Felipe de Paulo <depaulofelipe558@gmail.com>
2024-10-08 18:26:23 -0300

    Gulp directory deleted

--- gulp/source/scripts/greetings.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function (){
-    setTimeout(function (){
-        const name = prompt("Olá! Qual é o seu nome?");
-        alert(`Prazer te conhecer, ${name}!`);
-    }, 3000)
-})
